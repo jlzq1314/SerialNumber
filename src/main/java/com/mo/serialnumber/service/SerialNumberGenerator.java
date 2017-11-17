f02741112b3fbc2f248288dfef4731676a8b524b
xingwang.mo <xingwang.mo@chinaredstar.com>
2017-11-17 23:15:48 +0800

    16位订单号长度，接受平均没分钟每个用户下单数过万

--- src/main/java/com/mo/serialnumber/service/SerialNumberGenerator.java
+++ src/main/java/com/mo/serialnumber/service/SerialNumberGenerator.java
@@ -1,4 +1,4 @@
-package com.mo.serialnumber.service;
+package com.chinaredstar.ordercenter.test.serialnumber;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -6,52 +6,75 @@ import java.util.Calendar;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
+ * 订单号生成
  * @author MoXingwang on 2017-11-17.
  */
 public class SerialNumberGenerator {
     private static final int USER_ID_LENGTH = 5;
 
-    public static String generate(String prefix,String userId){
+    /**
+     * 订单号生成
+     * @param userId
+     * @return
+     */
+    public static String generate(String userId){
         if(StringUtils.isBlank(userId)){
-            userId = ThreadLocalRandom.current().nextLong(10000, 99999) + "";
+            userId = ThreadLocalRandom.current().nextLong((int)Math.pow(10, USER_ID_LENGTH - 1), (int)Math.pow(10, USER_ID_LENGTH) - 1) + "";
         }else if(userId.length() < USER_ID_LENGTH){
-            userId = addZeroForNum(userId,USER_ID_LENGTH);
+            userId = StringUtils.leftPad(userId,USER_ID_LENGTH,"0");
         }else if(userId.length() > USER_ID_LENGTH){
             userId = userId.substring(userId.length() - USER_ID_LENGTH,userId.length());
         }
 
         Calendar can = Calendar.getInstance();
         int year = can.get(Calendar.YEAR) - 2017;
-        int month = can.get(Calendar.MONTH) + 1;
         int days = can.get(Calendar.DAY_OF_YEAR);
+        int hour = can.get(Calendar.HOUR_OF_DAY);
+        int min = can.get(Calendar.MINUTE);
 
-        String req = (year * 12 + month) + "" + (days + ThreadLocalRandom.current().nextLong(370, 630));
+        int minutes = (hour - 8 < 0 ? 0 : hour - 8) * 60 + min;
 
-        long random = ThreadLocalRandom.current().nextLong(0, 99999999) ;
-        req +=  addZeroForNum(random + "",8);
+        String req = (year * 366 + days) + StringUtils.leftPad(minutes + "",3,"0");
 
-        if(null != prefix){
-            return prefix + req + userId;
-        }
+        req +=  StringUtils.leftPad(ThreadLocalRandom.current().nextLong(0, 999999) + "",6,"0");
+
         return req + userId;
     }
 
-    private static String addZeroForNum(String str, int strLength) {
-        int strLen = str.length();
-        StringBuffer sb = null;
-        while (strLen < strLength) {
-            sb = new StringBuffer();
-            sb.append(str).append("0");
-            str = sb.toString();
-            strLen = str.length();
-        }
-        return str;
+    /**
+     * 订单号生成
+     * @param prefix
+     * @param userId
+     * @return
+     */
+    public static String generate(String prefix,String userId){
+        return prefix + generate(userId);
     }
 
+
+    /**
+     * 推荐不再使用前缀
+     * @param args
+     */
     public static void main(String[] args) {
+        System.out.println(generate("11493677"));
+        System.out.println(generate("11493677"));
+        System.out.println(generate("11493677"));
+        System.out.println(generate("11493677"));
+        System.out.println(generate("","11493677"));
+        System.out.println(generate("","11493677"));
+        System.out.println(generate("","11493677"));
+        System.out.println(generate("","11493677"));
+        System.out.println(generate("","11493677"));
+        System.out.println(generate("","11493677"));
         System.out.println(generate("SO","11493677"));
         System.out.println(generate("SO","11493677"));
-        System.out.println(generate("SO","11111493677"));
+        System.out.println(generate("SO","11493677"));
+        System.out.println(generate("SO","11493677"));
+        System.out.println(generate("SO","11493677"));
+        System.out.println(generate("SO","11493677"));
+        System.out.println(generate("SO","11493677"));
+        System.out.println(generate("SO","1111149300677"));
         System.out.println(generate("SO","677"));
         System.out.println(generate("SO",""));
         System.out.println(generate("SO"," "));
